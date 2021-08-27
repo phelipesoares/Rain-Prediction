d908afc1e7f95f63fa2afe62b194f83540126409
phelipe-sb <69798348+phelipe-sb@users.noreply.github.com>
2021-08-26 22:41:53 -0300

    Create Get_Data.py

---
+++ Get_Data.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[134]:
+
+
+### Take the data from API
+
+import requests
+import time
+from datetime import datetime
+
+f = open(r"C:\Users\Phelipe\Desktop\The New Beginning\project_info.txt", "r")
+info_list = eval(f.read())
+
+for x in info_list[1]:
+    table_id = x
+    
+for x in info_list[2]:
+    path = x
+    
+for x in info_list[3]:
+    url = x
+    
+headers = info_list[0]
+
+cities = list(("Sao Paulo", "Carapicuiba", "New York", "Paris", "London", "Rome", "Moscow", "Ottawa", "Hong Kong", "Beijing"))
+listdict = list(())
+for i in cities:
+    querystring = {"q":"{}".format(i)}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    var = response.json()
+    dic = var['current']
+    dic['condition'] = dic['condition']['text']
+    dic['city'] = "{}".format(i)
+    listdict.append(dic)
+    time.sleep(1)
+    
+from google.cloud import bigquery
+from google.oauth2 import service_account
+credentials = service_account.Credentials.from_service_account_file(path)
+project_id = credentials.project_id
+client = bigquery.Client(project=project_id, credentials=credentials)
+print(f'list:{listdict}')
+
+input = []
+for x in listdict:
+    for i in x.values():
+        input.append(i)
+        if len(input) < 24:
+            pass
+        else:
+            rows_to_insert = [
+            {
+            u"last_updated_epoch": u"{}".format(input[0]), 
+            u"last_updated": u"{}".format(input[1]),
+            u"temp_c": u"{}".format(input[2]),
+            u"temp_f": u"{}".format(input[3]),
+            u"is_day": u"{}".format(input[4]),
+            u"condition": u"{}".format(input[5]),
+            u"wind_mph": u"{}".format(input[6]),
+            u"wind_kph": u"{}".format(input[7]),
+            u"wind_degree": u"{}".format(input[8]),
+            u"wind_dir": u"{}".format(input[9]),
+            u"pressure_mb": u"{}".format(input[10]),
+            u"pressure_in": u"{}".format(input[11]),
+            u"precip_mm": u"{}".format(input[12]),
+            u"precip_in": u"{}".format(input[13]),
+            u"humidity": u"{}".format(input[14]),
+            u"cloud": u"{}".format(input[15]),
+            u"feelslike_c": u"{}".format(input[16]),
+            u"feelslike_f": u"{}".format(input[17]),
+            u"vis_km": u"{}".format(input[18]),
+            u"vis_miles": u"{}".format(input[19]),
+            u"uv": u"{}".format(input[20]),
+            u"gust_mph": u"{}".format(input[21]),
+            u"gust_kph": u"{}".format(input[22]),
+            u"city": u"{}".format(input[23]),
+            u"extraction_date": u"{}".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+            },
+            ]
+            print(f'rows_to_insert:{rows_to_insert}')
+            errors = client.insert_rows_json(table_id, rows_to_insert)
+            input = []
+
+
+# In[133]:
+
+
+errors
+
