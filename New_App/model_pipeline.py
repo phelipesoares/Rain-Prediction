27d2784e89b2431b849289dd4a153c2159e302c8
phelipe-sb <69798348+phelipe-sb@users.noreply.github.com>
2022-04-06 00:24:48 -0300

    update with __main__ function

--- New_App/model_pipeline.py
+++ New_App/model_pipeline.py
@@ -11,42 +11,42 @@ from Functions.model_training import *
 
 # In[2]:
 
+def main():
+    client, project_id, credentials = gcp_credentials(path = r"C:\Users\Phelipe\Documents\GitHub\Rain-Prediction\App\Credentials\Weather Project-6fa8e059f642.json")
 
-client, project_id, credentials = gcp_credentials(path = r"C:\Users\Phelipe\Documents\GitHub\Rain-Prediction\App\Credentials\Weather Project-6fa8e059f642.json")
+    df = get_table(client, query_string = """
+      SELECT *
+      FROM (SELECT *
+                  , ROW_NUMBER() OVER(PARTITION BY last_updated, city ORDER BY last_updated, city) AS row_num
+                  FROM `weather-project-305419.Daily_Weather.Weather-Info`) as row_weather
+      WHERE row_num = 1
+      """)
 
-df = get_table(client, query_string = """
-  SELECT *
-  FROM (SELECT *
-              , ROW_NUMBER() OVER(PARTITION BY last_updated, city ORDER BY last_updated, city) AS row_num
-              FROM `weather-project-305419.Daily_Weather.Weather-Info`) as row_weather
-  WHERE row_num = 1
-  """)
+    df = cities_filter(df, cidades = ['Paris', 'Sao Paulo', 'Carapicuiba', 'New York', 'Otawwa', 'London', 'Rome', 'Moscow'
+              , 'Hong Kong', 'Beijing'])
 
-df = cities_filter(df, cidades = ['Paris', 'Sao Paulo', 'Carapicuiba', 'New York', 'Otawwa', 'London', 'Rome', 'Moscow'
-          , 'Hong Kong', 'Beijing'])
+    df = tag_target(df, raining_list = ['Light rain', 'Light rain shower', 'Light drizzle', 'Heavy rain', 'Moderate rain'
+      , 'Patchy light rain with thunder', 'Moderate or heavy rain shower', 'Patchy light rain', 'Torrential rain shower'
+      , 'Moderate rain at times', 'Moderate or heavy rain with thunder'])
 
-df = tag_target(df, raining_list = ['Light rain', 'Light rain shower', 'Light drizzle', 'Heavy rain', 'Moderate rain'
-  , 'Patchy light rain with thunder', 'Moderate or heavy rain shower', 'Patchy light rain', 'Torrential rain shower'
-  , 'Moderate rain at times', 'Moderate or heavy rain with thunder'])
+    df = to_float(df)
 
-df = to_float(df)
+    df = date_diff_calc(df)
 
-df = date_diff_calc(df)
+    df = rain_next1(df)
 
-df = rain_next1(df)
+    # Choose Variables
+    df = df[['city', 'temp_c', 'is_day', 'condition', 'wind_kph', 'wind_degree'
+              , 'pressure_mb', 'precip_mm', 'humidity', 'cloud', 'feelslike_c', 'vis_km', 'uv'
+              , 'gust_kph', 'target', 'target_1']]
 
-# Choose Variables
-df = df[['city', 'temp_c', 'is_day', 'condition', 'wind_kph', 'wind_degree'
-          , 'pressure_mb', 'precip_mm', 'humidity', 'cloud', 'feelslike_c', 'vis_km', 'uv'
-          , 'gust_kph', 'target', 'target_1']]
+    x, y = split_x_y(df)
 
-x, y = split_x_y(df)
+    model_pipe, preprocessor = training_pipe(x, y)
 
-model_pipe, preprocessor = training_pipe(x, y)
+    save_object(model_pipe, 'Rain_Model_Object')
 
-save_object(model_pipe, 'Rain_Model_Object')
-
-upload_blob(bucket_name='weather-ml-bucket',
-            source_file_name='Rain_Model_Object',
-            destination_blob_name='model/Rain_Model_Object')
+    upload_blob(bucket_name='weather-ml-bucket',
+                source_file_name='Rain_Model_Object',
+                destination_blob_name='model/Rain_Model_Object')
 
